64107e194e65017855e8e49f08fa3425f11c6e98
santiago-federici <santiagofederici88@gmail.com>
2024-08-26 20:11:38 -0300

    feat: created endpoint to send emails

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless'
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental:{
+    env: {
+      schema: {
+        RESEND_API_KEY: envField.string({
+          context: 'server',
+          access: 'secret',
+        }),
+      }
+    }
+  },
+  output: "server",
+  integrations: [react()],
+  adapter: vercel()
+});
